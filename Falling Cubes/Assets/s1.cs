7f1176e1f16c48fe01aab99307bb0e50cb3c0370
Sasha Burchuk <sasha.burchuk@instrument.com>
2016-08-18 15:02:45 -0700

    add some shaders and textures and things

---
+++ Falling Cubes/Assets/s1.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class s1 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log ("Start spinning!");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		gameObject.transform.Rotate (new Vector3 (0, 1, 0));
+	}
+}
